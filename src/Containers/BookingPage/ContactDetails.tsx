8fdc243ab85fa8be1c1b7c6ce3c0456bcff177d3
Batman <batman@Mac.fritz.box>
2025-12-16 15:49:52 +0100

    chore: update dependencies for build

--- src/Containers/BookingPage/ContactDetails.tsx
+++ src/Containers/BookingPage/ContactDetails.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styled from "styled-components";
 import { Container, Subtitle, type Contact, type ErrorType, type Touched } from "./BookingPage";
 import { media } from "../../styles/Theme";
